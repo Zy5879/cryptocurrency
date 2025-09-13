32eeb258ced75f4e07f77db4f61ba105e9f04818
Zaire McAllister <ZaireM21@gmail.com>
2025-09-13 18:58:59 -0400

    feat: add get coin by id

--- backend/src/routes/getCoin.ts
+++ backend/src/routes/getCoin.ts
@@ -12,10 +12,31 @@ getCoin.get("/", async (_req, res) => {
       body: undefined,
     };
 
-    const response = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd",
-      options
-    );
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      const text = await response.text();
+      return res.status(response.status).json({ error: text });
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+getCoin.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const url = `https://api.coingecko.com/api/v3/coins/${id}`;
+    const options = {
+      method: "GET",
+      headers: { "x-cg-demo-api-key": process.env.COINGECKOAPI as string },
+      body: undefined,
+    };
+
+    const response = await fetch(url, options);
 
     if (!response.ok) {
       const text = await response.text();
